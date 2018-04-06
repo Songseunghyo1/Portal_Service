fb942ff8867f7f323c4074be7ac936f816508b77
Songseunghyo1 <tmdgy15@gmail.com>
2018-04-06 15:41:16 +0900

    make build.gradle

--- 1주차/UserDaoRe/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ 1주차/UserDaoRe/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,5 +1,8 @@
 package kr.ac.jejunu;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class DaoFactory {
 
