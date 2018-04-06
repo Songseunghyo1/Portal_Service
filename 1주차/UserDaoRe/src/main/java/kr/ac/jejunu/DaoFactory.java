16bed0a646aa80949dbf4ac81a8b4713ebdf07eb
Songseunghyo1 <tmdgy15@gmail.com>
2018-04-06 15:38:58 +0900

    make build.gradle

--- 1주차/UserDaoRe/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ 1주차/UserDaoRe/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,11 +1,13 @@
 package kr.ac.jejunu;
 
+@Configuration
 public class DaoFactory {
 
-    public UserDao getUserDao() {
+    @Bean
+    public UserDao userDao() {
         return new UserDao(new JejuConnectionMaker());
     }
-
+    @Bean
     public ConnectionMaker getConnectionMaker() {
         return new JejuConnectionMaker();
     }
--- 1주차/UserDaoRe/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ 1주차/UserDaoRe/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -16,7 +18,8 @@ public class UserDaoTest {
 
     @Before
     public void setup() {
-        userDao = daoFactory.getUserDao();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
     }
 
     @Test
@@ -40,26 +43,4 @@ public class UserDaoTest {
         assertThat(insertedUser.getName(), is(user.getName()));
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
-
-    /*@Test
-    public void hallaGet() throws SQLException, ClassNotFoundException {
-        int id= 1;
-        User user = hallaUserDao.get(id);
-        assertThat(user.getId(), is(1));
-        assertThat(user.getName(), is("허윤호"));
-        assertThat(user.getPassword(), is("1234"));
-    }
-
-    @Test
-    public void hallaAdd() throws SQLException, ClassNotFoundException {
-        User user = new User();
-        user.setName("헐크");
-        user.setPassword("1111");
-        Integer id = hallaUserDao.insert(user);
-
-        User insertedUser = hallaUserDao.get(id);
-        assertThat(insertedUser.getId(), is(id));
-        assertThat(insertedUser.getName(), is(user.getName()));
-        assertThat(insertedUser.getPassword(), is(user.getPassword()));
-    }*/
 }
